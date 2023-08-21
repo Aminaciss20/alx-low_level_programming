69d40e356df6960632a4e3700d47b99181f06730
Aminaciss20 <kamoonano@gmail.com>
2023-08-21 12:58:24 -0700

    h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * rest_to_90 - takes a pointer to an int parameter
+ *              ans updates the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
